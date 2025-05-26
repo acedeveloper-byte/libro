45a9eefb5ec80556bf26e3cc32287080129d4da0
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-26 10:14:08 +0530

    added changr

--- app/contact-us/layout.js
+++ app/contact-us/layout.js
@@ -1,29 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "Contact Librovuelos | 24*7 Support",
-  description: "Need Help? Reach out to experts of Librovuelos, and seek assistance 24*7 regarding all travel-related queries.",
+	title: 'Contact Librovuelos | 24*7 Support',
+	description:
+		'Need Help? Reach out to experts of Librovuelos, and seek assistance 24*7 regarding all travel-related queries.',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
 }
