3dd300188fde2fcabcbb34e395618896e5de6ff7
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-26 10:11:52 +0530

    added change

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,35 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata = {
-  title: "Book Cheap Flights | Librovuelos",
-  description:
-    "Find exclusive deals on flight bookings and make affordable reservations. Call Librovuelos for deals, and travel for less!",
+	title: 'Book Flights | Librovuelos',
+	description:
+		'Find exclusive deals on flight bookings and make affordable reservations. Call Librovuelos for deals, and travel for less!',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta
-          name="google-site-verification"
-          content="pVaLl6JmQ2N5KO858MA28ltPiCr0tP-w1kRoS5ayX3Y"
-        />
-      </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<meta
+					name='google-site-verification'
+					content='pVaLl6JmQ2N5KO858MA28ltPiCr0tP-w1kRoS5ayX3Y'
+				/>
+			</head>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
 }
--- components/HomeAbout.js
+++ components/HomeAbout.js
@@ -1,59 +1,62 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import Image from "next/image";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomeContent = () => {
-  return (
-    <div>
-      <Container className="my-2">
-        <Row>
-          <Col md={6} className="mob-layout">
-            <Image
-              src="/images/home-about-poster.jpg"
-              alt=" Airlines"
-              width={550}
-              height={500}
-            />
-          </Col>
-          <Col md={6}>
-            <p className="fw-bold">Travel For Less! </p>
+	return (
+		<div>
+			<Container className='my-2'>
+				<Row>
+					<Col
+						md={6}
+						className='mob-layout'>
+						<Image
+							src='/images/home-about-poster.jpg'
+							alt=' Airlines'
+							width={550}
+							height={500}
+						/>
+					</Col>
+					<Col md={6}>
+						<p className='fw-bold'>Travel For Less! </p>
 
-            <h2 className="fs-3 fw-bold">
-            Librovuelos- Where Budget Meets the Skies
-
-            </h2>
-            <p>
-            At Librovuelos, we believe that exploring the world shouldn’t come with a hefty price tag. That’s why we’ve made it easy to search, compare, and book the cheapest flights from multiple airlines. If you are looking for the best travel experience within your budget, we can be your perfect pick. Decide where you want to go, set up a budget, and we will craft a perfect itinerary even for less. Connect with us to plan your long-awaited trip.
-
-            </p>
-            <h4>What You Get?</h4>
-            <ul>
-             
-              <li>
-                <b>
-              Exclusive Deals: </b>Get countless flight deals to snap up on the big savings.
-
-              </li>
-              <li>
-             <b> Personalized Experience:</b> Share your priorities, and customzie your journey accordingly.
-
-              
-              </li>
-              <li>
-              <b>Safe & Secure Bookings:</b> Book confidently with us, knowing all your data is safe.
-
-              </li>
-              <li>
-              <b>World-Class Services:</b> Choose us for top-notch travel booking services.
-
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+						<h2 className='fs-3 fw-bold'>
+							Librovuelos- Where Budget Meets the Skies
+						</h2>
+						<p>
+							At Librovuelos, we believe that exploring the world shouldn’t come
+							with a hefty price tag. That’s why we’ve made it easy to search,
+							compare, and book the flights from multiple airlines. If you are
+							looking for the best travel experience within your budget, we can
+							be your perfect pick. Decide where you want to go, set up a
+							budget, and we will craft a perfect itinerary even for less.
+							Connect with us to plan your long-awaited trip.
+						</p>
+						<h4>What You Get?</h4>
+						<ul>
+							<li>
+								<b>Exclusive Deals: </b>Get countless flight deals to snap up on
+								the big savings.
+							</li>
+							<li>
+								<b> Personalized Experience:</b> Share your priorities, and
+								customzie your journey accordingly.
+							</li>
+							<li>
+								<b>Safe & Secure Bookings:</b> Book confidently with us, knowing
+								all your data is safe.
+							</li>
+							<li>
+								<b>World-Class Services:</b> Choose us for top-notch travel
+								booking services.
+							</li>
+						</ul>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default HomeContent;
